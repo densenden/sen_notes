14c6f487f4d7ccd00e2310da26c2f4d8df6f330a
densenden <git@deniskreuzer.dk>
2025-03-24 15:53:46 +0100

    first ex

--- exercise1.js
+++ exercise1.js
@@ -1,10 +1,6 @@
 let readline = require('readline-sync');
 
-<<<<<<< HEAD
-// Your code here
-=======
 let color = readline.question('What is your favorite color? ');
 let fruit = readline.question('What is your favorite fruit? ');
 
 console.log('Your favorite color is ' + color + ' and your favorite fruit is ' + fruit + '.');
->>>>>>> 72ee63944b02c0a73c219f376dc3d12fad7a9c46
