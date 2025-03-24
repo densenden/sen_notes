72ee63944b02c0a73c219f376dc3d12fad7a9c46
densenden <git@deniskreuzer.dk>
2025-03-24 15:28:19 +0100

    first exercise tested

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+let readline = require('readline-sync');
+
+let color = readline.question('What is your favorite color? ');
+let fruit = readline.question('What is your favorite fruit? ');
+
+console.log('Your favorite color is ' + color + ' and your favorite fruit is ' + fruit + '.');
