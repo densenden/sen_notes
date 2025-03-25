0dab77cd903582f202cf3a40d8183bb5f07b934e
densenden <git@deniskreuzer.dk>
2025-03-25 16:29:02 +0100

    arrow function refactoring

--- inline_function.js
+++ inline_function.js
@@ -1,8 +1,6 @@
 let numbers = [2, 4, 5, 1, 6, 9, 8];
 
-function isGreaterThanFive(number) {
-    return number > 5;
-}
+let isGreaterThanFive = number)=> number > 5;
 
 let result = numbers.find(isGreaterThanFive);
 
